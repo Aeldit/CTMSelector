a15da5eddf4894367219a859dc4960af54a9baed
Aeldit <raphael.roger.92@gmail.com>
2023-09-06 21:09:59 +0200

    Folders can now be detected as packs

---
+++ changelogs/latest.md
@@ -0,0 +1,4 @@
+🫧 Changed :
+
+- CTM Packs file name no longer need to start with 'CTM' to be detected as a CTM pack
+- Zip files that were not packs could stille be detected as a CTM pack even if the file wasn't a pack at all
--- src/main/java/fr/aeldit/ctms/textures/FilesHandling.java
+++ src/main/java/fr/aeldit/ctms/textures/FilesHandling.java
@@ -25,6 +25,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,28 +34,38 @@ import java.util.Map;
 
 import static fr.aeldit.ctms.util.Utils.CTMS_OPTIONS_STORAGE;
 
-public class ConnectedTexturesHandling
+public class FilesHandling
 {
     private final Path resourcePacksDir = FabricLoader.getInstance().getGameDir().resolve("resourcepacks");
     private final String ctmPath = "assets/minecraft/optifine/ctm/connect/";
+    private final ArrayList<Path> folderPaths = new ArrayList<>();
 
     public void init()
     {
         CTMS_OPTIONS_STORAGE.clearOptionsMap();
 
+        if (!Files.exists(resourcePacksDir))
+        {
+            try
+            {
+                Files.createDirectories(resourcePacksDir);
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
         for (File zipFileOrFolder : resourcePacksDir.toFile().listFiles())
         {
-            // Zip File
-            if (zipFileOrFolder.isFile()
-                    && zipFileOrFolder.getName().startsWith("CTM")
-                    && zipFileOrFolder.getName().endsWith(".zip")
+            if (zipFileOrFolder.isFile() && zipFileOrFolder.getName().endsWith(".zip")
             )
             {
-                if (isCtmPack(zipFileOrFolder.toString()))
+                if (isZipCtmPack(zipFileOrFolder.toString()))
                 {
                     Map<String, Boolean> currentPackOptions = new HashMap<>();
 
-                    for (FileHeader fileHeader : listFilesInPack(zipFileOrFolder.toString()))
+                    for (FileHeader fileHeader : listFilesInZipPack(zipFileOrFolder.toString()))
                     {
                         if (fileHeader.toString().contains(ctmPath))
                         {
@@ -73,23 +84,49 @@ public class ConnectedTexturesHandling
                         }
                     }
 
-                    // If the array and the map are not empty, we initialise the options for the current pack
+                    // If the map is not empty, we initialise the options for the current pack
                     if (!currentPackOptions.isEmpty())
                     {
                         CTMS_OPTIONS_STORAGE.initPackOptions(zipFileOrFolder.getName(), currentPackOptions);
                     }
                 }
             }
+            else if (zipFileOrFolder.isDirectory() && isFolderCtmPack(zipFileOrFolder.getName()))
+            {
+                Map<String, Boolean> currentFolderPackOptions = new HashMap<>();
+
+                for (Path path : listFilesInFolderPack(zipFileOrFolder))
+                {
+                    if (path.toString().contains(ctmPath.replace("/", "\\")))
+                    {
+                        if (path.toString().endsWith(".properties"))
+                        {
+                            currentFolderPackOptions.put(path.getFileName().toString().replace(".properties", ""), true);
+                        }
+                        else if (path.toString().endsWith(".txt"))
+                        {
+                            currentFolderPackOptions.put(path.getFileName().toString().replace(".txt", ""), false);
+                        }
+                    }
+                }
+                folderPaths.clear();
+
+                // If the map is not empty, we initialise the options for the current pack
+                if (!currentFolderPackOptions.isEmpty())
+                {
+                    CTMS_OPTIONS_STORAGE.initPackOptions(zipFileOrFolder.getName(), currentFolderPackOptions);
+                }
+            }
         }
     }
 
-    private boolean isCtmPack(String packPath)
+    private boolean isZipCtmPack(String packPath)
     {
         try
         {
-            for (FileHeader fileHeader : listFilesInPack(packPath))
+            for (FileHeader fileHeader : listFilesInZipPack(packPath))
             {
-                if (fileHeader.toString().contains(ctmPath))
+                if (fileHeader.toString().startsWith("assets") && fileHeader.toString().contains(ctmPath))
                 {
                     return true;
                 }
@@ -102,7 +139,12 @@ public class ConnectedTexturesHandling
         return false;
     }
 
-    private List<FileHeader> listFilesInPack(String packPath)
+    private boolean isFolderCtmPack(String packPath)
+    {
+        return Files.exists(Path.of(resourcePacksDir + "\\" + packPath + "\\" + ctmPath));
+    }
+
+    private List<FileHeader> listFilesInZipPack(String packPath)
     {
         ArrayList<FileHeader> fileHeaders;
 
@@ -117,17 +159,33 @@ public class ConnectedTexturesHandling
         return fileHeaders;
     }
 
+    private ArrayList<Path> listFilesInFolderPack(@NotNull File packFolder)
+    {
+        for (File file : packFolder.listFiles())
+        {
+            if (file.isFile() && (file.getName().endsWith(".properties") || file.getName().endsWith(".txt")))
+            {
+                folderPaths.add(file.toPath());
+            }
+            else if (file.isDirectory())
+            {
+                listFilesInFolderPack(file);
+            }
+        }
+        return folderPaths;
+    }
+
     public void updateUsedTextures(@NotNull String packName)
     {
         // We disable the pack and reload the resources because the reloading makes the zip file accessible for writing
         MinecraftClient.getInstance().getResourcePackManager().disable("file/" + packName);
         MinecraftClient.getInstance().reloadResources();
 
-        if (isCtmPack(resourcePacksDir + "\\" + packName))
+        if (isZipCtmPack(resourcePacksDir + "\\" + packName))
         {
             Map<String, String> fileNamesMap = new HashMap<>();
 
-            for (FileHeader fileHeader : listFilesInPack(resourcePacksDir + "\\" + packName))
+            for (FileHeader fileHeader : listFilesInZipPack(resourcePacksDir + "\\" + packName))
             {
                 boolean option = CTMS_OPTIONS_STORAGE.getOption(packName, fileHeader.toString()
                         .split("/")[fileHeader.toString().split("/").length - 1]
