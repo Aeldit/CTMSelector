da479a2e386f0b34ea453a8001ea5b686ebf1ced
aeldit <raphael.roger.92@gmail.com>
2024-04-13 19:49:49 +0200

    Add a function to write the ctm_selector.json file inside a zip file

--- src/main/java/fr/aeldit/ctms/util/Utils.java
+++ src/main/java/fr/aeldit/ctms/util/Utils.java
@@ -3,10 +3,15 @@ package fr.aeldit.ctms.util;
 import fr.aeldit.ctms.textures.CTMPacks;
 import fr.aeldit.ctms.textures.FilesHandling;
 import net.fabricmc.loader.api.FabricLoader;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Utils
 {
@@ -17,6 +22,34 @@ public class Utils
     public static CTMPacks CTM_PACKS;
 
     public static final Path RESOURCE_PACKS_DIR = FabricLoader.getInstance().getGameDir().resolve("resourcepacks");
+
+    /**
+     * @param packPath     The path to the zip file
+     * @param headersBytes A map containing the fileHeaders as keys and the bytes to write in them as values
+     */
+    public static void writeBytesToZip(String packPath, @NotNull HashMap<String, byte[]> headersBytes)
+    {
+        // Mounts the zip file and adds files to it using the FileSystem
+        // The bytes written in the files are the ones we obtain
+        // from the properties files
+        HashMap<String, String> env = new HashMap<>();
+        env.put("create", "true");
+        Path path = Paths.get(packPath);
+        URI uri = URI.create("jar:" + path.toUri());
+
+        try (FileSystem fs = FileSystems.newFileSystem(uri, env))
+        {
+            for (Map.Entry<String, byte[]> entry : headersBytes.entrySet())
+            {
+                Path nf = fs.getPath(entry.getKey());
+                Files.write(nf, entry.getValue(), StandardOpenOption.CREATE);
+            }
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
 
     //public static int ICON_INDEX = 0;
 }
