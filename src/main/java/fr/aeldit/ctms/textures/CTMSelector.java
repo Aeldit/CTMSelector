9e67740a6030141d0826f25355d2531f478da192
aeldit <raphael.roger.92@gmail.com>
2024-08-09 17:44:05 +0200

    Fix the 0.png file not being used in priority

--- src/main/java/fr/aeldit/ctms/textures/CTMSelector.java
+++ src/main/java/fr/aeldit/ctms/textures/CTMSelector.java
@@ -275,14 +275,17 @@ public class CTMSelector
             return "";
         }
 
+        // The file 0.png is usually the block with all the borders, so we use this file if it exists
         for (File file : groupDir)
         {
-            // The file 0.png is usually the block with all the borders, so we use this file if it exists
             if (file.isFile() && file.toString().endsWith("0.png"))
             {
                 return file.toString();
             }
+        }
 
+        for (File file : groupDir)
+        {
             if (file.isFile() && file.getName().endsWith(".png"))
             {
                 return file.toString();
