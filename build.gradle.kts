755e464d173df503b1c3c8d12383f55dc7e54734
aeldit <aeldit.66@proton.me>
2025-05-03 12:09:58 +0300

    Add support for 1.21.5 + change build script to only take the MC version from the gradle.properties files

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,31 +15,70 @@ repositories {
     }
 }
 
-object Constants {
+object C {
     const val MOD_VERSION: String = "0.4.0"
     const val LOADER_VERSION: String = "0.16.10"
+    val YARN_MAPPINGS: HashMap<String, String> = hashMapOf(
+        "1.20.2" to "1.20.2+build.4",
+        "1.20.4" to "1.20.4+build.3",
+        "1.20.6" to "1.20.6+build.3",
+        "1.21.1" to "1.21.1+build.3",
+        "1.21.3" to "1.21.3+build.2",
+        "1.21.4" to "1.21.4+build.8",
+        "1.21.5" to "1.21.5+build.1",
+    )
+    val RANGES_NAMES: HashMap<String, String> = hashMapOf("1.21.1" to "1.21-1.21.1", "1.21.3" to "1.21.2-1.21.3")
+    val MODRINTH_VERSIONS: HashMap<String, String> = hashMapOf("1.21.1" to "1.21 1.21.1", "1.21.3" to "1.21.2 1.21.3")
+    val FABRIC_VERSIONS: HashMap<String, String> = hashMapOf(
+        "1.20.2" to "0.91.6+1.20.2",
+        "1.20.4" to "0.97.2+1.20.4",
+        "1.20.6" to "0.100.8+1.20.6",
+        "1.21-1.21.1" to "0.115.6+1.21.1",
+        "1.21.2-1.21.3" to "0.114.0+1.21.3",
+        "1.21.4" to "0.119.2+1.21.4",
+        "1.21.5" to "0.121.0+1.21.5",
+    )
+    val MOD_MENU_VERSIONS: HashMap<String, String> = hashMapOf(
+        "1.20.2" to "8.0.1",
+        "1.20.4" to "9.2.0-beta.2",
+        "1.20.6" to "10.0.0",
+        "1.21-1.21.1" to "11.0.3",
+        "1.21.2-1.21.3" to "12.0.0",
+        "1.21.4" to "13.0.3",
+        "1.21.5" to "14.0.0-rc.2",
+    )
+    val CONTINUITY_VERSIONS: HashMap<String, String> = hashMapOf(
+        "1.20.2" to "3.0.0+1.20.2",
+        "1.20.4" to "3.0.0+1.20.2",
+        "1.20.6" to "3.0.0+1.20.5",
+        "1.21-1.21.1" to "3.0.0+1.21",
+        "1.21.2-1.21.3" to "3.0.0+1.21.3",
+        "1.21.4" to "3.0.0+1.21.4",
+        "1.21.5" to "3.0.1-test.4+1.21.5",
+    )
 }
 
 class ModData {
-    val hasVersionRange = properties.containsKey("range_name")
-
     val mcVersion = property("minecraft_version").toString()
+    val yarnMappings = C.YARN_MAPPINGS[mcVersion]
 
-    val rangedName = if (hasVersionRange) property("range_name").toString() else mcVersion
-    private val modrinthVersions = if (hasVersionRange) property("modrinth_versions") else mcVersion
-    val versionsList = modrinthVersions.toString().split(" ")
+    val isj21 = mcVersion !in setOf("1.20.2", "1.20.4")
+    val javaVersion = if (isj21) "21" else "17"
+
+    val hasVersionRange = C.RANGES_NAMES.containsKey(mcVersion)
+
+    val rangedName = C.RANGES_NAMES.getOrDefault(mcVersion, mcVersion)
+
+    val versionsList = C.MODRINTH_VERSIONS.getOrDefault(mcVersion, mcVersion).split(" ")
     val min = if (hasVersionRange) versionsList[0] else mcVersion
     val max = if (hasVersionRange) versionsList[versionsList.size - 1] else mcVersion
 
-    val fabricVersion = property("fabric_version").toString()
-    val modmenuVersion = property("modmenu_version").toString()
-    val continuityVersion = property("continuity_version").toString()
+    val fabricVersion = C.FABRIC_VERSIONS[rangedName]
+    val modmenuVersion = C.MOD_MENU_VERSIONS[rangedName]
 
-    val fullVersion = "${Constants.MOD_VERSION}+${rangedName}"
+    val fullVersion = "${C.MOD_VERSION}+$rangedName"
 
-    val isj21 = mcVersion !in listOf("1.19.4", "1.20.1", "1.20.2", "1.20.4")
-
-    val javaVersion = if (isj21) "21" else "17"
+    val continuityVersion = C.CONTINUITY_VERSIONS[rangedName]
 }
 
 val mod = ModData()
@@ -52,8 +91,8 @@ base {
 
 dependencies {
     minecraft("com.mojang:minecraft:${mod.mcVersion}")
-    mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${Constants.LOADER_VERSION}")
+    mappings("net.fabricmc:yarn:${mod.yarnMappings}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${C.LOADER_VERSION}")
 
     modImplementation("net.fabricmc.fabric-api:fabric-api:${mod.fabricVersion}")
 
@@ -109,19 +148,25 @@ if (stonecutter.current.isActive) {
 tasks {
     processResources {
         inputs.property("version", mod.fullVersion)
-        inputs.property("loader_version", Constants.LOADER_VERSION)
         inputs.property("min", mod.min)
         inputs.property("max", mod.max)
         inputs.property("java_version", mod.javaVersion)
+        inputs.property("loader_version", C.LOADER_VERSION)
+        inputs.property("fabric_api_version", mod.fabricVersion)
+        inputs.property("modmenu_version", mod.modmenuVersion)
+        inputs.property("continuity_version", mod.continuityVersion)
 
         filesMatching("fabric.mod.json") {
             expand(
                 mapOf(
                     "version" to mod.fullVersion,
-                    "loader_version" to Constants.LOADER_VERSION,
                     "min" to mod.min,
                     "max" to mod.max,
-                    "java_version" to mod.javaVersion
+                    "java_version" to mod.javaVersion,
+                    "loader_version" to C.LOADER_VERSION,
+                    "fabric_api_version" to mod.fabricVersion,
+                    "modmenu_version" to mod.modmenuVersion,
+                    "continuity_version" to mod.continuityVersion
                 )
             )
         }
@@ -142,7 +187,7 @@ publishMods {
         accessToken = System.getenv("MODRINTH_TOKEN")
 
         projectId = "6OpnBWtt"
-        displayName = "[${mod.rangedName}] CTM Selector ${Constants.MOD_VERSION}"
+        displayName = "[${mod.rangedName}] CTM Selector ${C.MOD_VERSION}"
         version = mod.fullVersion
         type = STABLE
 
